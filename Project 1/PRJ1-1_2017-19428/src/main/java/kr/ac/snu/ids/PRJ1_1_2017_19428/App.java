3b96db100807e229c0882f410f0e89388df27778
givenone <givenone1@gmail.com>
2020-04-06 17:33:17 +0900

    Add project 1

---
+++ Project 1/PRJ1-1_2017-19428/src/main/java/kr/ac/snu/ids/PRJ1_1_2017_19428/App.java
@@ -0,0 +1,13 @@
+package kr.ac.snu.ids.PRJ1_1_2017_19428;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Project 1/PRJ1-1_2017-19428/src/test/java/kr/ac/snu/ids/PRJ1_1_2017_19428/AppTest.java
@@ -0,0 +1,20 @@
+package kr.ac.snu.ids.PRJ1_1_2017_19428;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
